292184e24531463a94bfebf5f8bd5a41510a4113
Erick <erick_aug@hotmail.com>
2017-11-22 17:38:49 -0200

    Grafo

---
+++ src/componentesfortes/Constantes.java
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentesfortes;
+
+public class Constantes {
+    public static final int INFINITO = 999999;    
+}
