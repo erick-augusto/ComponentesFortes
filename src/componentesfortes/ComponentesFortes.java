31f43a783394754c93f30adafe8db245ed70cd5a
Erick <erick_aug@hotmail.com>
2017-11-30 10:13:34 -0200

    Main

---
+++ src/componentesfortes/ComponentesFortes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentesfortes;
+
+/**
+ *
+ * @author charles
+ */
+public class ComponentesFortes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Leitura do teclado
+        Grafo g = new Grafo();
+        g.leDoTeclado();
+        g.imprimeNaTela();
+    }
+    
+}
