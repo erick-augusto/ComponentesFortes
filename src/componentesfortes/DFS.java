1fd4a4778e927e7b776d5c6ac13f401e4bf3508b
Erick <erick_aug@hotmail.com>
2017-11-30 10:23:54 -0200

    Acha Componentes

--- src/componentesfortes/DFS.java
+++ src/componentesfortes/DFS.java
@@ -8,5 +8,19 @@ public class DFS {
     }
     
     private int tempo = 0;
-
+    
+    public int achaComponetes(Grafo g){
+        int i;    
+        for (i = 0; i < g.vertices.length; i++) {
+            g.vertices[i].cor = Cores.BRANCO;
+        }
+        for (i = 0; i < g.vertices.length; i++) {
+            if(g.vertices[i].cor == Cores.BRANCO){
+                //System.out.println("Entrando na recursão...");
+                visitaBFS(g, g.vertices[i]);
+            }
+        }       
+        return tempo;
+    }
+    
 }
